589be28fc3b2538d1f3661beb78dc47a7c019110
danny castrillon <dannycastrillon2@hotmail.com>
2024-04-27 00:12:39 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('productos', 'ProductoController');
+Route::resource('proveedores', 'ProveedorController');
+Route::resource('ordenes-compra', 'OrdenCompraController');
