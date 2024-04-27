f05194f4a65ed71afddceb0569890506525a97bf
danny castrillon <dannycastrillon2@hotmail.com>
2024-04-26 23:48:49 -0500

    OrdenCompra.M

---
+++ app/Models/OrdenCompra.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrdenCompra extends Model
+{
+    protected $fillable = ['producto_id', 'proveedor_id', 'cantidad', 'fecha_orden', 'fecha_entrega_estimada'];
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
+
+    public function proveedor()
+    {
+        return $this->belongsTo(Proveedor::class);
+    }
+}
