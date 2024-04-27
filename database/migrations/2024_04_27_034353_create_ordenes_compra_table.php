41cf4b01a7789cb8a67e0cbf30aa2bb691e7ce04
danny castrillon <dannycastrillon2@hotmail.com>
2024-04-26 23:31:56 -0500

    commit 3

--- database/migrations/2024_04_27_034353_create_ordenes_compra_table.php
+++ database/migrations/2024_04_27_034353_create_ordenes_compra_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOrdenesCompraTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('ordenes_compra', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('producto_id');
+            $table->unsignedBigInteger('proveedor_id');
+            $table->integer('cantidad');
+            $table->date('fecha_orden');
+            $table->date('fecha_entrega_estimada');
             $table->timestamps();
         });
     }
