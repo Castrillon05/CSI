a07cef96c97d9e119a0907b8f78299ed991e205b
danny castrillon <dannycastrillon2@hotmail.com>
2024-04-27 00:10:39 -0500

    Proveedores.C

---
+++ app/Http/Controllers/ProveedorController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proveedor;
+use Illuminate\Http\Request;
+
+class ProveedorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $proveedores = Proveedor::all();
+        return view('proveedores.index', compact('proveedores'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('proveedores.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Proveedor::create($request->all());
+        return redirect()->route('proveedores.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Proveedor $proveedor)
+    {
+        return view('proveedores.show', compact('proveedor'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Proveedor $proveedor)
+    {
+        return view('proveedores.edit', compact('proveedor'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Proveedor $proveedor)
+    {
+        $proveedor->update($request->all());
+        return redirect()->route('proveedores.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Proveedor $proveedor)
+    {
+        $proveedor->delete();
+        return redirect()->route('proveedores.index');
+    }
+}
